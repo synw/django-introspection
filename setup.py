1e8846a44ae3318992254c72b2496edb8ee26d1b
synw <synwx@protonmail.com>
2021-08-25 12:11:34 +0200

    Add long description to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+from os import path
 from setuptools import setup, find_packages
 
 
 version = __import__("introspection").__version__
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="django-introspection",
@@ -9,6 +14,8 @@ setup(
     include_package_data=True,
     version=version,
     description="Introspection tools for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="synw",
     author_email="synwe@yahoo.com",
     url="https://github.com/synw/django-introspection",
