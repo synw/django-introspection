cad08e93fde5e6c66e87774a8aaae718c8f03f11
synw <synwx@protonmail.com>
2021-11-02 16:40:35 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,32 +1,5 @@
-from os import path
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-
-version = __import__("introspection").__version__
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-setup(
-    name="django-introspection",
-    packages=find_packages(),
-    include_package_data=True,
-    version=version,
-    description="Introspection tools for Django",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="synw",
-    author_email="synwe@yahoo.com",
-    url="https://github.com/synw/django-introspection",
-    download_url="https://github.com/synw/django-introspection/releases/tag/" + version,
-    keywords=["django"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Django :: 3.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
-    ],
-    zip_safe=False,
-)
+setup()
